7c6f06fa2fdc35a817d40de046745b01b7b2433c
verhulstm <michael@terminallabs.com>
2020-12-31 15:55:56 -0600

    cleaning

--- setup.py
+++ setup.py
@@ -1,25 +1,67 @@
+import os
 import sys
+import pathlib
+import configparser
+from pathlib import Path
+from os.path import join, basename, abspath, isdir, isfile, dirname
 from setuptools import setup, find_packages
 
-from spinglass.settings import *
+assert sys.version_info >= (3, 6, 0)
 
-assert sys.version_info >= MINIMUM_PYTHON_VERSION
+with open(os.path.dirname(__file__) + "/src/framework/loader.py") as f:
+    code = compile(f.read(), "loader.py", "exec")
+    exec(code)
+
+_path = str(pathlib.Path(__file__).parent.absolute())
+_src = "src"
+_config = "/setup.cfg"
+
+package_link = ".tmp/symlink"
+
+config = configparser.ConfigParser()
+config.read(_path + _config)
+version = config["metadata"]["version"]
+name = config["metadata"]["name"]
+
+repo_name = name
+package_name = repo_name.replace("-", "")
+setup_stub_name = package_name
+setup_full_name = repo_name
+setup_description = setup_full_name.replace("-", " ")
+
+setup_author = ("Terminal Labs",)
+setup_author_email = ("solutions@terminallabs.com",)
+setup_license = ("see LICENSE file",)
+setup_url = "https://github.com/terminal-labs/" + package_name
+
+setup_links(package_name)
+
+pins = []
+
+reqs = [
+    "setuptools",
+]
+
+extras = [
+    "utilities-package-pinion@git+https://gitlab.com/terminallabs/utilitiespackage/utilities-package-pinion.git",
+    "utilities-package@git+https://gitlab.com/terminallabs/utilitiespackage/utilities-package.git",
+]
 
 setup(
-    name="spinglass",
-    version=VERSION,
-    author="Terminal Labs",
-    author_email="solutions@terminallabs.com",
-    license="mit",
-    packages=find_packages(),
+    name=package_name,
+    version=version,
+    description=setup_description,
+    url=setup_url,
+    author=setup_author,
+    author_email=setup_author_email,
+    license=setup_license,
+    package_dir={"": package_link},
+    packages=find_packages(where=package_link),
     zip_safe=False,
     include_package_data=True,
-    install_requires=[
-        "setuptools",
-        "standardmodel@git+https://github.com/terminal-labs/standardmodel.git",
-    ],
+    install_requires=pins + reqs + smart_reqs(extras, package_name),
     entry_points="""
         [console_scripts]
-        spinglass=spinglass.cli:main
-     """,
+    """
+    + f"{setup_stub_name}={setup_stub_name}.__main__:main",
 )
